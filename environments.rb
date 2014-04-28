d49cc4f9dda5bb49ddcf6a41a82002b4c9d58638
Michael Arledge <arledgemike@gmail.com>
2014-04-28 12:22:04 -0500

    f dbs's

--- environments.rb
+++ environments.rb
@@ -4,7 +4,7 @@ configure :development do
 end
 
 configure :production do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost:5432/postgres')
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://qchpngtedqreqj:duWMP7LJWhAmymwUO7x_scGN6B@ec2-54-221-223-92.compute-1.amazonaws.com:5432/d9s4lg1unsugtu')
 
 	ActiveRecord::Base.establish_connection(
 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
