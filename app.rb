29023351bfaaa0882135093fa56ae9c12aa9a070
ArledgeMike <arledgemike@gmail.com>
2014-04-29 20:11:21 -0500

    seperate admin tool in views

--- app.rb
+++ app.rb
@@ -2,19 +2,26 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-
 class Cut < ActiveRecord::Base
-
 end
-
 
 set :username,'Bond'
 set :token,'shakenN0tstirr3d'
 set :password,'007'
 
 helpers do
-  def admin? ; request.cookies[settings.username] == settings.token ; end
-  def protected! ; halt [ 401, 'Not Authorized' ] unless admin? ; end
+  
+  def admin? 
+    request.cookies[settings.username] == settings.token 
+  end
+  
+  def protected!
+    halt [ 401, 'Not Authorized' ] unless admin?
+  end
+  
+  include Rack::Utils
+  alias_method :h, :escape_html
+  
 end
 
 get "/" do
@@ -26,71 +33,72 @@ get "/" do
   end
 end
 
-get "/new" do
-  protected!
-  erb :new
-end
-
 get "/cut_that/:id" do
   @id = params[:id]
   erb :cut, :layout => :base
 end
 
-get "/list" do
+get "/logged_in/new" do
+  protected!
+  erb :'logged_in/new', :layout => :'logged_in/logged_in'
+end
+
+get "/logged_in/list" do
   protected!
   @cut = Cut.all
-  erb :list
+  erb :'logged_in/list', :layout => :'logged_in/logged_in'
 end
 
-post "/new" do
+post "/logged_in/new" do
   @cut = Cut.create(:title => params[:title], :body => params[:body])
-  redirect "/"
+  redirect "/logged_in/list"
 end
 
-get "/delete/:id" do
+get "/logged_in/delete/:id" do
   protected!
   @cut = Cut.find(params[:id])
-  erb :delete
+  erb :'logged_in/delete', :layout => :'logged_in/logged_in'
 end
 
-delete "/delete/:id" do
+delete "/logged_in/delete/:id" do
   Cut.delete params[:id]
-  redirect "/"
+  redirect "/logged_in/list"
 end
 
-get "/update/:id" do
+get "/logged_in/update/:id" do
   protected!
   @cut = Cut.find params[:id]
-  erb :update
+  erb :'logged_in/update', :layout => :'logged_in/logged_in'
 end
 
-patch "/update/:id" do
+patch "/logged_in/update/:id" do
   @cut = Cut.find params[:id]
   @cut.title = params[:title]
   @cut.body = params[:body]
   @cut.save
-  redirect "/"
+  redirect "/logged_in/list"
 end
 
 get "/admin" do
-erb :admin
+  unless admin?
+    
+    erb :'admin'
+    #erb :menu, :layout => :logged_in
+  else
+    redirect "/logged_in/list" 
+  end
 end
 
-post '/login' do
+post '/logged_in/login' do
   if params['username']==settings.username&&params['password']==settings.password
     response.set_cookie(settings.username,settings.token) 
-    redirect '/'
+    redirect '/logged_in/list'
   else
     "Username or Password incorrect"
   end
 end
 
-get "/logout" do 
- response.set_cookie(settings.username, false)
- redirect '/' 
-end
-
-helpers do
-  include Rack::Utils
-  alias_method :h, :escape_html
+get "/logged_in/logout" do 
+  response.set_cookie(settings.username, false)
+  redirect '/' 
 end
